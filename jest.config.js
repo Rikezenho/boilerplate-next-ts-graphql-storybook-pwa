f0bb9e6be863e5bdceb43c8eb08cb23a76e20103
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-04-02 04:36:22 -0300

    change main component

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  // moduleNameMapper: {
+  //   'styled-components':
+  //     '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  // }
 }
