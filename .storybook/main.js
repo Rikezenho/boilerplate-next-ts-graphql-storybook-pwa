15220cdebd3d928db24e76e17044dd52b24e0fef
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-04-02 05:34:32 -0300

    remove storybook knobs and use essentials instead

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: [
-    '@storybook/addon-knobs',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials'
-  ]
+  addons: ['@storybook/addon-essentials']
 }
