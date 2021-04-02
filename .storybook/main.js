ea9b899274d05da0beb664531d97a318e07b624e
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-04-02 05:12:30 -0300

    added storybook and first story

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/components/**/stories.tsx'],
+  addons: [
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
+  ]
+}
