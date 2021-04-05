80321805405be6d73f5f8bf1beded6c22691fe3e
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-04-05 00:17:41 -0300

    add pwa config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple boilerplate with TS, React, NextJS and Styled Components"
